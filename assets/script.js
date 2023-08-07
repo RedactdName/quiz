2b9aba24ff23e1b46f8923fedbe95eb2ccfb8e24
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-08-07 17:46:15 -0600

    starting fresh with html, css & js

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,60 @@
-/* Initial webpage opens with title and display of high scores
-    insturcitonal prompt explains rules of game with start button to press
-    card to right of screen lists top scores and initials 
-    create cards with question 
-    randomizer to present questions
-    first question displays after hitting start on prompt
-    
-    Four answer options appear and timer starts to count down
-    create timer
-    display 4 mulitple choice style answers
-    create submit button
-    
-    Player answers question correctly and it moves on to the next question
-    new card appears with a different question an 4 answers
-    
-    Player answers question incorrectly and the time gets deducted 
-    prompt of incorrect answer
-    time is deducted from remaining time left in quiz
-    
-    If player goes through all questions before timer runs out prompt players score 
-    prompt announcing score
-    
-    When timer ends GAME OVER
-    GAME OVER appears with score and prompt to add high score or start again
-    
-    Prompt to enter initials to add to high score list 
-    */
+//Arrays 
+var questions = [ 
+    {
+    question: "1.What is the capital city of Alaska?",
+    choices: ['Anchorage', 'Juno', 'Kenai', 'Fairbanks'],
+    answer: "Juno"
+},
+{
+    question: "2.What year ws the first Barbie doll ?",
+    choices: ['1957', '1958', '1959', '1960'],
+    answer: "1959"
+},
+{
+    question: "3. ?",
+    choices: ['', '', '', ''],
+    answer: ""
+},
+{
+    question: "4.?",
+    choices: ['', '', '', ''],
+    answer: ""
+},
+{
+    question: "5.What is the capital city of Alaska?",
+    choices: ['Anchorage', 'Juno', 'Kenai', 'Fairbanks'],
+    answer: "Juno"
+}
+]
+
 //Variables
-var timer = document.querySelector(".timer");
-var startBtn = document.querySelector(".start-btn");
-var question1 = document.querySelector(".card1");
-var question2 = document.querySelector(".card2");
-var question3 = document.querySelector(".card3");
-var question4 = document.querySelector(".card4");
-var questions = [question1, question2, question3, question4]
-var questionCounter = 0;
-var score = document.querySelector(".score");
-var secondsLeft = 45;
+const timer = document.getElementById("countdown")
+const intro = document.getElementById("intro")
+// const quiz = document.getElementById("quiz")
+const question = document.getElementById("question")
+const choices = documetn.getElementById("choices")
+const results = document.getElementById("score")
+// const ansA = document.getElementById("A")
+// const ansB = document.getElementById("B")
+// const ansC = document.getElementById("C")
+// const ansD = document.getElementById("D")
+const high = document.getElementById("scoreContainer")
+var startBtn = document.querySelector(".start-btn")
 
-//Functions
-// function timer 
-// for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-
-// }
-
-
-//Special Functions
+//event listener
 startBtn.addEventListener("click", function () {
-    question1.classList.remove("hidden")
+    //hide intro
+    intro.classList.add("hidden")
     startBtn.classList.add("hidden")
 })
-
-const btns = document.querySelectorAll('.ans', 'answer')
-
-// When player clicks start button first question appears
-btns.forEach(btn => {
-    //event listener to make questions appear/disapper individually and consecutively
-    btn.addEventListener('click', event => {
-        alert(event.target.textContent);
-        questionCounter++
-        if (questionCounter == 1) {
-            question1.classList.add("hidden")
-            question2.classList.remove("hidden")
-        }
-        if (questionCounter == 2) {
-            question2.classList.add("hidden")
-            question3.classList.remove("hidden")
-        }
-        if (questionCounter == 3) {
-            question3.classList.add("hidden")
-            question4.classList.remove("hidden")
-        }
-        if (questionCounter == 4) {
-            question4.classList.add("hidden")
-            score.classList.remove("hidden")
-        }
-    });
-
-});
-//Logic
+//function 
+//unhide questions
+questions.classList.remove("hidden")
+        // start timer for the clock (function)
+    function clock() {
+        
+    }
+ 
+    //call out second function
+// create clock function()
+// create 2nd fucntion() -- display a question & process a for loop for choices to display as buttons
---
+++ assets/script2.js
@@ -0,0 +1,97 @@
+/* Initial webpage opens with title and display of high scores
+    insturcitonal prompt explains rules of game with start button to press
+    card to right of screen lists top scores and initials 
+    create cards with question 
+    randomizer to present questions
+    first question displays after hitting start on prompt
+    
+    Four answer options appear and timer starts to count down
+    create timer
+    display 4 mulitple choice style answers
+    create submit button
+    
+    Player answers question correctly and it moves on to the next question
+    new card appears with a different question an 4 answers
+    
+    Player answers question incorrectly and the time gets deducted 
+    prompt of incorrect answer
+    time is deducted from remaining time left in quiz
+    
+    If player goes through all questions before timer runs out prompt players score 
+    prompt announcing score
+    
+    When timer ends GAME OVER
+    GAME OVER appears with score and prompt to add high score or start again
+    
+    Prompt to enter initials to add to high score list 
+    */
+//Variables
+const timer = document.getElementById("countdown")
+const quiz = document.getElementById("quiz")
+const question = document.getElementById("question")
+const ansA = document.getElementById("A")
+const ansB = document.getElementById("B")
+const ansC = document.getElementById("C")
+const ansD = document.getElementById("D")
+const high = document.getElementById("scoreContainer")
+var startBtn = document.querySelector(".start-btn")
+
+//Functions 
+var questions = [ {
+    question: "1.What is the capital city of Alaska?",
+    choices: ['Anchorage', 'Juno', 'Kenai', 'Fairbanks'],
+    correctOption: "b"
+}]
+
+var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+        //     } else if (timeLeft === 1) {
+        //       // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        //       timerEl.textContent = timeLeft + ' second remaining';
+        //       timeLeft--;
+    } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        //       // Call the `displayMessage()` function
+        //       displayMessage();
+    }
+}, 450000);
+
+
+//Special Functions
+startBtn.addEventListener("click", function () {
+    quiz.classList.remove("hidden")
+    startBtn.classList.add("hidden")
+})
+
+const btns = document.querySelectorAll('.ans', 'answer')
+
+// When player clicks start button first question appears
+btns.forEach(btn => {
+    //event listener to make questions appear/disapper individually and consecutively
+    btn.addEventListener('click', event => {
+        alert(event.target.textContent);
+        questionCounter++
+        if (questionCounter == 1) {
+            question1.classList.add("hidden")
+            question2.classList.remove("hidden")
+        }
+        if (questionCounter == 2) {
+            question2.classList.add("hidden")
+            question3.classList.remove("hidden")
+        }
+        if (questionCounter == 3) {
+            question3.classList.add("hidden")
+            question4.classList.remove("hidden")
+        }
+        if (questionCounter == 4) {
+            question4.classList.add("hidden")
+            score.classList.remove("hidden")
+        }
+    });
+
+});
+//Logic
